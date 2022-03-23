5b4752832f5033b364bcb6d411c9c7429f193897
neddypratama <neddy_pratama@yahoo.com>
2022-03-23 12:42:46 +0700

    Minggu 6 - Neddy Pratama Wiryawan

---
+++ cabang.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+    </head>
+    <body>
+        <?php
+            $nama = "Neddy";
+
+            if($nama=="Andi") {
+                echo "Selamat Belajar $nama";
+            }
+        ?>
+    </body>
+</html>
